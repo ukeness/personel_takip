80f512b8df47579398f5bed1348cf5d5f668be3f
enesk <eneskilci999@gmail.com>
2025-07-15 12:49:36 +0300

    checkout clean_arch

--- root/app.ts
+++ root/app.ts
@@ -18,7 +18,7 @@ app.set("view engine", "ejs")
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(process.cwd() +"/root/public"))
+app.use(express.static("public"))
 console.log(process.cwd())
 
 app.use("/admin", adminRoutes)
